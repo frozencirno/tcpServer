c87ba70f3be4f2e04cc3f5d432dfc2d9d9d8500c
Mysticalwaters <mysticlh@outlook.com>
2025-08-02 14:26:05 +0100

    Added multithreading to the message handling

--- client.c
+++ client.c
@@ -1,12 +1,20 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <stdio.h>
+#include <string.h>
+
 
 #pragma comment(lib, "Ws2_32.lib")
 
 #define DEFAULT_PORT 27015
 
+/* TODO
+    Add the ability to send files like images and compressed files
+    Add a graphical user interface
+*/
+
 int main() {
+    //Init WSA
     int iResult;
     WSADATA wsaData;
     iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
@@ -14,17 +22,19 @@ int main() {
         fprintf(stderr, "WSAStartup Failed: %d\n", iResult);
         return 1;
     }
+    //Create the socket which is used to create a "path" for connections
     SOCKET client_fd = socket(AF_INET, SOCK_STREAM, 0);
     if(client_fd == INVALID_SOCKET) {
         fprintf(stderr, "Failed to create the socket: %d\n", WSAGetLastError());
         WSACleanup();
         return 1;
     }
+    //Create a structure describing the information of the server so we can locate it and understand what protocol it is using.
     struct sockaddr_in serverAddr = {0};
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(DEFAULT_PORT);
     inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-    
+    //Simply connect to the server
     iResult = connect(client_fd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
     if(iResult == INVALID_SOCKET) {
         fprintf(stderr, "Connect() Failed: %d", WSAGetLastError());
@@ -35,18 +45,27 @@ int main() {
 
     printf("Connection Success!\n");
 
-    const char* message = "Hello world!";
-    iResult = send(client_fd, message, strlen(message), 0);
-    if(iResult == SOCKET_ERROR) {
-        fprintf(stderr, "Failed to send message%d\n", WSAGetLastError());
-        return 1;
-    } else {
-        printf("Sent successfully\n");
+    //Loop handles user input and passes data onto server.
+    while (1)
+    {
+        char message[1024];
+        printf("Enter message (or 'exit' to quit): ");
+        fgets(message, 1024, stdin);
+        //Remove the newline so we can display it correct when sent and for when we compare it to "exit"
+        message[strcspn(message, "\n")] = '\0';
+        if(strcmp(message, "exit") == 0) {
+            break;
+        }
+        //Send the string to the server so it can be displayed and stored.
+        iResult = send(client_fd, message, strlen(message), 0);
+        if(iResult == SOCKET_ERROR) {
+            fprintf(stderr, "Failed to send message%d\n", WSAGetLastError());
+            return 1;
+        } else {
+            printf("Sent successfully\n");
+        }
     }
-
-    printf("Press enter to quit.");
-    getchar();
-
+    //Cleanup
     closesocket(client_fd);
     WSACleanup();
     return 0;
--- server.c
+++ server.c
@@ -2,10 +2,41 @@
 #include <ws2tcpip.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <process.h>
 
 #pragma comment(lib, "Ws2_32.lib")
 
 #define DEFAULT_PORT 27015
+
+/* TODO
+    Multithreading so the server can handle more than one client at a time.
+    User accounts(Encryption as well)
+*/
+
+unsigned __stdcall ClientHandler(void* client_socket) {
+    SOCKET client_fd = *(SOCKET*)client_socket;
+    free(client_socket);
+    char buffer[1024];
+    int bytes_recived;
+    while((bytes_recived = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
+        buffer[bytes_recived] = '\0';
+        printf("Received: %s\n", buffer);
+    }
+    if(bytes_recived == 0) {
+        printf("Client disconnected gracefully.\n");
+    } else {
+        fprintf(stderr, "recv failed: %d\n", WSAGetLastError());
+    }
+
+    closesocket(client_fd);
+    return 0;
+}
+
+void HandleConsoleCommands(char* command) {
+    if(strlwr(command) == "help") {
+        printf("debug message");
+    }
+}
 
 int main() {
     FILE *errorFile = freopen("serverErrorLog.txt", "w", stderr);
@@ -38,26 +69,38 @@ int main() {
 
     listen(server_fd, 5);
     printf("Server listening on port %d...\n", DEFAULT_PORT);
+    printf("Enter help for a list of commands.\n");
 
     while (1)
     {
-        SOCKET client_fd = accept(server_fd, NULL, NULL);
-        if(client_fd == INVALID_SOCKET) {
+        struct sockaddr_in client_addr;
+        int client_addr_len = sizeof(client_addr);
+
+        SOCKET* client_fd = (SOCKET*)malloc(sizeof(SOCKET));
+        if (!client_fd) {
+            fprintf(stderr, "malloc failed\n");
+            continue;
+        }
+    
+        *client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_addr_len);
+        if(*client_fd == INVALID_SOCKET) {
             fprintf(stderr, "Accept failed! %d\n", WSAGetLastError());
             continue;
         }
-        printf("New Client connected\n");
-        char buffer[1024];
-        int bytes_recived;
-        while((bytes_recived = recv(client_fd, buffer, sizeof(buffer), 0)) > 0) {
-            buffer[bytes_recived] = '\0';
-            printf("Received: %s\n", buffer);
-        }
-        if(bytes_recived == 0) {
-            printf("Client disconnected gracefully.\n");
-        } else {
 
+        char client_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+        
+        printf("New Client connected from %s:%d\n", client_ip, ntohs(client_addr.sin_port));
+        HANDLE thread = (HANDLE)_beginthreadex(NULL, 0, &ClientHandler, client_fd, 0, NULL);
+        if (thread == NULL) {
+            fprintf(stderr, "Error creating thread\n");
+            closesocket(*client_fd);
+            free(client_fd);
+        } else {
+            CloseHandle(thread);
         }
+        
     }
     fclose(errorFile);
     closesocket(server_fd);
